ecff3eba262a5294a850655a4bf9c5f9119f7747
matthewbeta <hello@matthew-jackson.com>
2014-03-12 23:12:58 +0000

    added routes for authentication

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,46 @@
-module.exports = function (app) {
+module.exports = function (app, passport) {
 
 	app.get('/', function(req, res) {
 		res.render('index');
 	});
+
+	// route middleware to verify this (the isLoggedIn function)
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile', {
+			user : req.user // get the user out of session and pass to template
+		});
+	});
+
+	app.get('/auth/twitter', passport.authenticate('twitter'));
+
+	// handle the callback after twitter has authenticated the user
+	app.get('/auth/twitter/callback',
+		passport.authenticate('twitter', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+	}));
+
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+	// send to twitter to do the authentication
+	app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
+	app.get('/connect/twitter/callback',
+		passport.authorize('twitter', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+	}));
 };
+
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
