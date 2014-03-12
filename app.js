a9a58fae9afd16a7aeb212af8dc3da7150f37482
matthewbeta <hello@matthew-jackson.com>
2014-03-12 23:18:08 +0000

    adding DB and Passort config to app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+require("./config/passport")(passport);
 
 app.configure(function () {
 	app.use(express.logger('dev'));
@@ -8,7 +14,12 @@ app.configure(function () {
 	app.use(express.bodyParser());
 	app.set('view engine', 'jade');
 
-	require("./app/routes.js")(app); 
+	// required for passport
+	app.use(express.session({ secret: 'busta rhymes island'})); //session secret
+	app.use(passport.initialize()); 
+	app.use(passport.session()); //prrsistent logns
+
+	require("./app/routes.js")(app, passport); 
 
 	app.listen(port);
 
